1ca808aa5d53bdf05c7d66891970aed5775d2dbe
Shripada Mahato <mahatoshripada692@gmail.com>
2026-08-15 19:09:03 +0530

    actually start learning

--- module3.sql
+++ module3.sql
@@ -1,1 +1,28 @@
-DROP database company, company1;
+
+DROP database company;
+-- CREATE DATABASE
+CREATE DATABASE company;
+
+-- SELECT DATABASE
+USE company;
+
+-- CREATE TABLE
+CREATE TABLE student(
+    id INT PRIMARY KEY,
+    name VARCHAR(50),
+    age INT,
+    course VARCHAR(50),
+    marks DECIMAL(5,2)
+);
+
+-- INSERT DATA
+INSERT INTO student
+(id, name, age, course, marks)
+VALUES
+(1, 'Shripada', 21, 'BCA', 75.50);
+
+-- VIEW DATA
+SELECT * FROM student;
+
+-- VIEW STRUCTURE
+DESC student;
