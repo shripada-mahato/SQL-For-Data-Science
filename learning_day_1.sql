8c8ccfba82f039a3e59610675af08bfd577a0552
Shripada Mahato <mahatoshripada692@gmail.com>
2026-08-16 12:22:42 +0530

    day-1, 2nd time learning

---
+++ learning_day_1.sql
@@ -0,0 +1,33 @@
+-- 1st time learning --
+
+SHOW DATABASES;
+-- use the database using USE command
+USE student;
+CREATE TABLE student2(
+    name VARCHAR(50),
+    roll INT,
+    dept VARCHAR(20)
+);
+INSERT INTO student2(name,roll,dept)
+VALUES
+('Arjun',60,'BCA'),
+('Neha',61,'MCA'),
+('Rohit',62,'BCA'),
+('Anjali',63,'BCA');
+SELECT * FROM student2;
+-- print specific column by using WHERE
+SELECT * FROM student2 WHERE dept = 'BCA';
+SELECT * FROM student2 WHERE roll < 63;
+
+-- 2nd time learning --
+
+-- print specific Data by using WHERE, AND, OR, NOT, function
+SELECT * FROM student2 WHERE dept = 'BCA';
+SELECT * FROM student2 WHERE roll = 61;
+SELECT * FROM student2 WHERE roll > 61;
+SELECT * FROM student2 WHERE roll < 61;
+SELECT * FROM student2 WHERE roll >= 61;
+SELECT * FROM student2 WHERE roll <= 61;
+SELECT * FROM student2 WHERE roll > 60 AND dept = 'BCA';
+SELECT * FROM student2 WHERE dept = 'MCA' OR roll = 63;
+SELECT * FROM student2 WHERE NOT dept = 'MCA';
